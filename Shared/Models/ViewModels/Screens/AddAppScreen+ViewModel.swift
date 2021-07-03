d0a96e283f4a660187c5d0ad512a3aee845bc7a0
kamaal111 <kamaal.f1@gmail.com>
2021-07-03 20:02:32 +0200

    Adding an validator

--- Shared/Models/ViewModels/Screens/AddAppScreen+ViewModel.swift
+++ Shared/Models/ViewModels/Screens/AddAppScreen+ViewModel.swift
@@ -9,6 +9,8 @@ import Foundation
 import Combine
 import ShrimpExtensions
 
+// - MARK: - View Model
+
 extension AddAppScreen {
     final class ViewModel: ObservableObject {
 
@@ -26,30 +28,15 @@ extension AddAppScreen {
             }
         }
 
-        enum FormValidationErrors: Error {
-            case appNameMissing
-            case invalidAppIdentifier
-            case invalidAccessToken
-
-            var alertMessage: AlertMessage {
-                switch self {
-                case .appNameMissing:
-                    // - TODO: LOCALIZE THIS
-                    return AlertMessage(title: "App name is missing")
-                case .invalidAppIdentifier:
-                    // - TODO: LOCALIZE THIS
-                    return AlertMessage(title: "Invalid app identifier")
-                case .invalidAccessToken:
-                    return AlertMessage(title: "Invalid access token")
-                }
-            }
-        }
-
         func onDoneEditing() {
             do {
-                try validateForm()
+                try Validator.validateForm([
+                    .appIdentifier: appIdentifier,
+                    .appName: appName,
+                    .accessToken: accessToken
+                ])
             } catch {
-                if let error = error as? FormValidationErrors {
+                if let error = error as? Validator.Errors {
                     alertMessage = error.alertMessage
                 }
                 return
@@ -57,17 +44,68 @@ extension AddAppScreen {
             print("done")
         }
 
-        private func validateForm() throws {
-            guard !appName.trimmingByWhitespacesAndNewLines.isEmpty else {
-                throw FormValidationErrors.appNameMissing
-            }
-            guard appIdentifier.trimmingByWhitespacesAndNewLines.split(separator: ".").count > 1 else {
-                throw FormValidationErrors.invalidAppIdentifier
-            }
-            guard accessToken.trimmingByWhitespacesAndNewLines.count == 32 else {
-                throw FormValidationErrors.invalidAccessToken
+    }
+}
+
+// - MARK: - Validator
+
+extension AddAppScreen {
+    fileprivate struct Validator {
+        private init() { }
+
+        static func validateForm(_ form: [Fields: String]) throws {
+            for field in form {
+                try field.key.validate(value: field.value)
             }
         }
+    }
+}
 
+extension AddAppScreen.Validator {
+    fileprivate enum Fields {
+        case appName
+        case appIdentifier
+        case accessToken
+
+        func validate(value: String) throws {
+            switch self {
+            case .appName:
+                guard !value.trimmingByWhitespacesAndNewLines.isEmpty else {
+                    throw Errors.appNameMissing
+                }
+            case .appIdentifier:
+                guard value.trimmingByWhitespacesAndNewLines.split(separator: ".").count > 1 else {
+                    throw Errors.invalidAppIdentifier
+                }
+            case .accessToken:
+                guard value.trimmingByWhitespacesAndNewLines.count == 32 else {
+                    throw Errors.invalidAccessToken
+                }
+            }
+        }
+    }
+
+    fileprivate enum Errors: Error {
+        case appNameMissing
+        case invalidAppIdentifier
+        case invalidAccessToken
+        case general
+
+        var alertMessage: AlertMessage {
+            switch self {
+            case .appNameMissing:
+                // - TODO: LOCALIZE THIS
+                return AlertMessage(title: "App name is missing")
+            case .invalidAppIdentifier:
+                // - TODO: LOCALIZE THIS
+                return AlertMessage(title: "Invalid app identifier")
+            case .invalidAccessToken:
+                // - TODO: LOCALIZE THIS
+                return AlertMessage(title: "Invalid access token")
+            case .general:
+                // - TODO: LOCALIZE THIS
+                return AlertMessage(title: "Something went wrong")
+            }
+        }
     }
 }
