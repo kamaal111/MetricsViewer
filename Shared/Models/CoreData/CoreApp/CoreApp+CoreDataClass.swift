a983e82021174c95e85ddbc7fc8ddc9c86aa92f8
Kamaal <kamaal@tranzer.com>
2021-07-06 09:08:49 +0200

    Defining app core data model

---
+++ Shared/Models/CoreData/CoreApp/CoreApp+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CoreApp+CoreDataClass.swift
+//  MetricsViewer
+//
+//  Created by Kamaal Farah on 06/07/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreApp)
+public class CoreApp: NSManagedObject {
+
+}
---
+++ Shared/Models/CoreData/CoreApp/CoreApp+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  CoreApp+CoreDataProperties.swift
+//  MetricsViewer
+//
+//  Created by Kamaal Farah on 06/07/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CoreApp {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CoreApp> {
+        return NSFetchRequest<CoreApp>(entityName: "CoreApp")
+    }
+
+    @NSManaged public var accessToken: String?
+    @NSManaged public var appIdentifier: String?
+    @NSManaged public var creationDate: Date?
+    @NSManaged public var id: UUID?
+    @NSManaged public var name: String?
+    @NSManaged public var updateDate: Date?
+
+}
+
+extension CoreApp : Identifiable {
+
+}
