1974b2c7a760db02c4dc0ebab7f248e87f769560
kamaal111 <kamaal.f1@gmail.com>
2021-07-10 21:50:43 +0200

    Removing unused code

--- Shared/Models/ViewModels/Screens/HomeScreen+ViewModel.swift
+++ Shared/Models/ViewModels/Screens/HomeScreen+ViewModel.swift
@@ -6,34 +6,9 @@
 //
 
 import Foundation
-import MetricsNetworker
 import Combine
-import XiphiasNet
 
 extension HomeScreen {
     final class ViewModel: ObservableObject {
-
-        @Published var root: RootResponse? {
-            didSet {
-                guard let root = root else { return }
-                print(root)
-            }
-        }
-
-        private let networkController = NetworkController.shared
-
-        func getRoot() {
-            networkController.getRoot(completion: { (result: Result<RootResponse?, XiphiasNet.Errors>) in
-                switch result {
-                case .failure(let failure): print(failure)
-                case .success(let success):
-                    guard let success = success else { return }
-                    DispatchQueue.main.async { [weak self] in
-                        self?.root = success
-                    }
-                }
-            })
-        }
-
     }
 }
