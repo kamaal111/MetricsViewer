3349fba54ce60fd26fbabeab80f096c6a428de16
Kamaal <kamaal@tranzer.com>
2021-07-22 09:02:24 +0200

    Saving hosts in their manager

--- Shared/Models/ViewModels/Environment/CoreHostManager.swift
+++ Shared/Models/ViewModels/Environment/CoreHostManager.swift
@@ -23,8 +23,21 @@ final class CoreHostManager: ObservableObject {
         }
     }
 
-    func addHost(_ host: CoreHost) {
-        hosts.append(host)
+    func saveHost(_ args: CoreHost.Args) -> CoreHost? {
+        guard let context = persistenceController.context else {
+            console.error(Date(), "no context found")
+            return nil
+        }
+        let hostResult = CoreHost.setHost(with: args, context: context)
+        let host: CoreHost
+        switch hostResult {
+        case .failure(let failure):
+            console.error(Date(), failure.localizedDescription, failure)
+            return nil
+        case .success(let success): host = success
+        }
+        addHost(host)
+        return host
     }
 
     func fetchAllHosts() {
@@ -37,6 +50,10 @@ final class CoreHostManager: ObservableObject {
         case .success(let success): hosts = success ?? []
         }
         self.hosts = hosts
+    }
+
+    private func addHost(_ host: CoreHost) {
+        hosts.append(host)
     }
 
 }
--- Shared/Views/SupportingViews/ModifyApp.swift
+++ Shared/Views/SupportingViews/ModifyApp.swift
@@ -6,11 +6,8 @@
 //
 
 import SwiftUI
-import MetricsLocale
 import SalmonUI
 import MetricsUI
-import PersistanceManager
-import ConsoleSwift
 
 struct ModifyApp: View {
     @EnvironmentObject
@@ -59,8 +56,7 @@ struct ModifyApp: View {
                 name: $viewModel.editingHostName,
                 urlString: $viewModel.editingHostURLString,
                 onSave: {
-                if let savedHost = viewModel.onHostSave() {
-                    coreHostManager.addHost(savedHost)
+                if let hostArgs = viewModel.onHostSave(), let savedHost = coreHostManager.saveHost(hostArgs) {
                     selectedHost = savedHost
                 }
             },
@@ -72,137 +68,6 @@ struct ModifyApp: View {
                     let selectedHost = coreHostManager.hosts.first(where: { $0.name == newValue }) else { return }
             self.selectedHost = selectedHost
         })
-    }
-}
-
-extension ModifyApp {
-    final class ViewModel: ObservableObject {
-        @Published var showHostSheet = false
-        @Published var editingHostName = ""
-        @Published var editingHostURLString = ""
-        @Published var selectedHostName: String
-        @Published var showAlert = false
-        @Published private(set) var alertMessage: AlertMessage? {
-            didSet { alertMessageDidSet() }
-        }
-
-        private let persistenceController: PersistanceManager
-
-        init(selectedHostName: String, preview: Bool = false) {
-            self.selectedHostName = selectedHostName
-            if !preview {
-                self.persistenceController = PersistenceController.shared
-            } else {
-                self.persistenceController = PersistenceController.preview
-            }
-        }
-
-        func onHostSave() -> CoreHost? {
-            guard let context = persistenceController.context else {
-                console.error(Date(), "no context found")
-                return nil
-            }
-            let validatorResult = HostValidator.validateForm([
-                .name: editingHostName,
-                .url: editingHostURLString
-            ], context: context)
-            switch validatorResult {
-            case .failure(let failure):
-                alertMessage = failure.alertMessage
-                return nil
-            case .success: break
-            }
-            // Allready being validated in `HostValidator.validateForm`
-            let hostURL = URL(string: editingHostURLString)!
-            let args = CoreHost.Args(url: hostURL, name: editingHostName)
-            // - TODO: Move this to core host manager
-            let hostResult = CoreHost.setHost(with: args, context: context)
-            let host: CoreHost
-            switch hostResult {
-            case .failure(let failure):
-                console.error(Date(), failure.localizedDescription, failure)
-                return nil
-            case .success(let success): host = success
-            }
-            closeHostSheet()
-            selectedHostName = host.name
-            return host
-        }
-
-        func onAddHostButtonPress() {
-            showHostSheet = true
-        }
-
-        func closeHostSheet() {
-            showHostSheet = false
-            editingHostName = ""
-            editingHostURLString = ""
-        }
-
-        private func alertMessageDidSet() {
-            if !showAlert && alertMessage != nil {
-                showAlert = true
-            } else if showAlert && alertMessage == nil {
-                showAlert = false
-            }
-        }
-    }
-}
-
-// - MARK: - Host Validator
-
-extension ModifyApp {
-    fileprivate struct HostValidator {
-        private init() { }
-
-        static func validateForm(_ form: [Fields: String], context: NSManagedObjectContext) -> Result<Void, Errors> {
-            for (key, value) in form {
-                let result = key.validate(value: value, context: context)
-                switch result {
-                case .failure(let failure): return .failure(failure)
-                case .success: continue
-                }
-            }
-            return .success(Void())
-        }
-    }
-}
-
-extension ModifyApp.HostValidator {
-    fileprivate enum Fields {
-        case name
-        case url
-
-        func validate(value: String, context: NSManagedObjectContext) -> Result<Void, Errors> {
-            switch self {
-            case .name:
-                if value.trimmingByWhitespacesAndNewLines.isEmpty {
-                    return .failure(.nameMissing)
-                }
-                if CoreHost.findHost(by: .name, of: value, context: context) != nil {
-                    return .failure(.nameNotUnique)
-                }
-            case .url:
-                if value.trimmingByWhitespacesAndNewLines.isEmpty, URL(string: value) == nil {
-                    return .failure(.invalidURL)
-                }
-            }
-            return .success(Void())
-        }
-    }
-
-    fileprivate enum Errors: AlertMessageError {
-        case nameMissing
-        case nameNotUnique
-        case invalidURL
-
-        var alertMessage: AlertMessage {
-            switch self {
-            case .nameMissing: return AlertMessage(title: .NAME_MISSING_ALERT_TITLE)
-            case .invalidURL: return AlertMessage(title: .INVALID_URL_ALERT_TITLE)
-            case .nameNotUnique: return AlertMessage(title: .NAME_NOT_UNIQUE_ALERT_TITLE)
-            }
-        }
     }
 }
 
